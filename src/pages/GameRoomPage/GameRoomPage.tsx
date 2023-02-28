c3ca7a5ddc7111a92291020473bff1247e879f89
marcosjanuario <januario.marcos@icloud.com>
2023-02-28 07:24:29 +0100

    created bingoBoard + generating matrix + checking bingo + responsive board

--- src/pages/GameRoomPage/GameRoomPage.tsx
+++ src/pages/GameRoomPage/GameRoomPage.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import './GameRoomPage.css';
+import React, { useContext } from 'react';
+import './GameRoomPage.scss';
 import GameTable from './GameTable/GameTable';
 import BingoDrum from './BingoDrum/BingoDrum';
+import BingoBoard from '../../components/BingoBoard/BingoBoard';
+import Context from '../../store/Players/Context';
+import { Player } from '../../utils/types';
 
-const GameRoomPage = (): JSX.Element => (
-  <div className="game-room-wrapper bg-image">
-    <GameTable>
-      <BingoDrum />
-    </GameTable>
-  </div>
-);
+const GameRoomPage = (): JSX.Element => {
+  const playersCtx = useContext(Context);
+
+  return (
+    <div className="game-room-wrapper bg-image">
+      <GameTable>
+        <BingoDrum />
+      </GameTable>
+      {playersCtx.players.map((player: Player) => (
+        <GameTable key={player.id}>
+          <div className="player-table-wrapper">
+            <div className="player-table-header" style={{ backgroundColor: player.stoneColor }}>
+              <span>{player.name}</span>
+            </div>
+            <BingoBoard />
+          </div>
+        </GameTable>
+      ))}
+    </div>
+  );
+};
 
 export default GameRoomPage;
--- src/utils/methods.tsx
+++ src/utils/methods.tsx
@@ -3,3 +3,97 @@ export const generateRandomId = (): string => {
   window.crypto.getRandomValues(array);
   return `${array[0]}${array[1]}`;
 };
+
+export const createUnorderedMatrix = (): (string | number)[][] => {
+  // Initialize the 5x5 matrix with empty strings
+  const matrix: (string | number)[][] = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+  ];
+
+  // Generate an array of numbers from 1 to 24 in random order
+  const numbers = [];
+  for (let i = 1; i <= 24; i++) {
+    numbers.push(i);
+  }
+  numbers.sort(() => Math.random() - 0.5); // Shuffle the array randomly
+
+  // Place the shuffled numbers in the matrix, skipping the middle element
+  let counter = 0;
+  const middleIndex = Math.floor(matrix.length / 2);
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === middleIndex && j === middleIndex) {
+        matrix[i][j] = '';
+      } else {
+        matrix[i][j] = numbers[counter];
+        counter++;
+      }
+    }
+  }
+
+  return matrix;
+};
+
+export const hasBingo = (matrix: any[][], nums: number[]) => {
+  // Check rows
+  for (let i = 0; i < matrix.length; i++) {
+    let count = 0;
+    let emptyCount = 0;
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === '') {
+        emptyCount++;
+      } else if (nums.includes(matrix[i][j])) {
+        count++;
+      }
+    }
+    if (count + emptyCount === matrix[i].length && count >= 4) {
+      return true;
+    }
+  }
+
+  // Check columns
+  for (let i = 0; i < matrix.length; i++) {
+    let count = 0;
+    let emptyCount = 0;
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[j][i] === '') {
+        emptyCount++;
+      } else if (nums.includes(matrix[j][i])) {
+        count++;
+      }
+    }
+    if (count + emptyCount === matrix.length && count >= 4) {
+      return true;
+    }
+  }
+
+  // Check diagonals
+  let diag1Count = 0;
+  let diag1EmptyCount = 0;
+  let diag2Count = 0;
+  let diag2EmptyCount = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][i] === '') {
+      diag1EmptyCount++;
+    } else if (nums.includes(matrix[i][i])) {
+      diag1Count++;
+    }
+    if (matrix[i][matrix.length - i - 1] === '') {
+      diag2EmptyCount++;
+    } else if (nums.includes(matrix[i][matrix.length - i - 1])) {
+      diag2Count++;
+    }
+  }
+  if (diag1Count + diag1EmptyCount === matrix.length && diag1Count >= 4) {
+    return true;
+  }
+  if (diag2Count + diag2EmptyCount === matrix.length && diag2Count >= 4) {
+    return true;
+  }
+
+  return false;
+};
